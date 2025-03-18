35fc78bce857858c10864a675156b0f75cdd63b1
Sanchit Jain <sanchit.jain1412@gmail.com>
2025-03-18 21:59:01 +0530

    Updated jest and ts config to use modules

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }]
+  }
 };
